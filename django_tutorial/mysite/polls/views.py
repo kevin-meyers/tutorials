843332132e8618f50f5cb5b35c4e305f92a69f58
Kevin <kevinm1776@gmail.com>
2019-11-17 11:57:28 -0800

    Finished page 3.

--- django_tutorial/mysite/polls/views.py
+++ django_tutorial/mysite/polls/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Question
 
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {'latest_questions': latest_questions}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f'You\'re looking at question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+
+    return render(request, 'polls/detail.html', context)
+
 
 def results(request, question_id):
     return HttpResponse(f'You\'re looking at the results of question {question_id}')
